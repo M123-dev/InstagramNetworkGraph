f717add25dced0e6f996d7e80024fa8e9fe49acc
Marvin M <39344769+M123-dev@users.noreply.github.com>
2022-07-31 20:58:43 +0200

    feat: Allow mock data generation

--- d3-page/code.js
+++ d3-page/code.js
@@ -1,4 +1,84 @@
 (async () => {
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  const mock_version = params.mock_version === "true";
+  let config_obj;
+
+  
+  let graph;
+
+  if (mock_version) {
+    console.log("Generating mock data");
+    config_obj = { "username": "Root", "attraceForce": -400 };
+
+    let obj = {
+      nodes: [],
+      links: [],
+    };
+
+    const alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+
+    for (let i = 0; i < alphabet.length; i++) {
+      obj.nodes.push({
+        id: alphabet[i],
+        size: Math.floor(Math.random() * 100),
+      });
+    }
+
+      obj.nodes.push({
+        id: "Root",
+        size: 100,
+      });
+
+      for (let i = 0; i < alphabet.length; i++) {
+        let random = Math.floor(Math.random() * (alphabet.length - 1)) / 3;
+        obj.links.push({
+          target: alphabet[i],
+          source: "Root",
+          length: Math.floor(Math.random() * 10) + 600,
+        });
+        for (let j = 0; j < random; j++) {
+          obj.links.push({
+            source: alphabet[i],
+            target: alphabet[j],
+            length: Math.floor(Math.random() * 10) + 600,
+          });
+        
+      }
+    }
+    graph = obj;
+  }
+
   let svg = d3.select("svg"),
     width = +svg.node().getBoundingClientRect().width,
     height = +svg.node().getBoundingClientRect().height;
@@ -7,10 +87,11 @@
 
   var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-  let response = await fetch("config.json"); 
-  let json = await response.json(); 
-  let root_username = json["username"];
-  let attraceForce = json["attraceForce"];
+  if (config_obj === undefined) {
+    config_obj = await (await fetch("config.json")).json();
+  }
+  let root_username = config_obj["username"];
+  let attraceForce = config_obj["attraceForce"];
 
   var simulation = d3
     .forceSimulation()
@@ -35,135 +116,138 @@
         })
     );
 
-
-
-
   console.log(root_username);
 
-  d3.json("data.json", function (error, graph) {
-    if (error) throw error;
+  if (graph === undefined) {
+    console.log("Loading json...");
+    const d3_response = await fetch("data.json");
+    graph = await d3_response.json();
+  
+    console.log("Loaded data.json");
+  }
 
-    var link = g
-      .selectAll("line")
-      .data(graph.links)
-      .enter()
-      .append("line")
-      .style("stroke-width", function (d) {
-        return d.value;
-      })
-      .style("stroke", function (d) {
-        if (d.source === root_username) {
-          return "#f54242";
-        } else {
-          return "#999";
-        }
-      })
-      .style("opacity", "1")
-      .attr("group", function (d) {
-        return d.group;
-      });
-
-    var node = g
-      .attr("class", "nodes")
-      .selectAll("g")
-      .data(graph.nodes)
-      .enter()
-      .append("g")
-      .call(
-        d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended)
-      );
-
-    var circle = node
-      .append("circle")
-      .attr("r", function (d) {
-        return d.size;
-      })
-      .attr("fill", "#fff")
-      .style("stroke-width", 2)
-      .style("stroke", function (d) {
-        return color(d.color);
-      })
-      .on("mouseenter", (evt, d) => {
-        let list = [];
-        link
-          //.attr("display", "none")
-          .style("opacity", "0.2")
-          .filter((l) => {
-            if (l.source.index === d) {
-              list.push(l.target.id);
-              return true;
-            } else if (l.target.index === d) {
-              list.push(l.source.id);
-              return true;
-            }
-            return false;
-          })
-          .style("opacity", "1");
-
-        node
-          .style("opacity", "0.4")
-          .filter((n) => {
-            return n.index === d || list.includes(n.id);
-          })
-          .style("opacity", "1");
-      })
-      .on("mouseleave", (evt) => {
-        //link.attr("display", "block");
-        link.style("opacity", "1");
-        node.style("opacity", "1");
-      });
-
-    // Adding the label for each node
-    var text = node
-      .append("text")
-      .attr("dy", ".35")
-      .attr("dx", 12)
-      .attr("class", "unselectable")
-      .text(function (d) {
-        return d.id;
-      })
-      .attr("text-anchor", "middle");
-
-    node.append("title").text(function (d) {
-      return d.id;
+  var link = g
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .style("stroke-width", function (d) {
+      return d.value;
+    })
+    .style("stroke", function (d) {
+      if (d.source === root_username) {
+        return "#f54242";
+      } else {
+        return "#999";
+      }
+    })
+    .style("opacity", "1")
+    .attr("group", function (d) {
+      return d.group;
     });
 
-    simulation.nodes(graph.nodes).on("tick", ticked);
+  var node = g
+    .attr("class", "nodes")
+    .selectAll("g")
+    .data(graph.nodes)
+    .enter()
+    .append("g")
+    .call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
 
-    simulation.force("link").links(graph.links);
-
-    // Finding connected people, usefull for later adding line highlighting
-    function neighboring(a, b) {
-      return graph.links.some(function (d) {
-        return (
-          (d.source.id === a.source.id && d.target.id === b.target.id) ||
-          (d.source.id === b.source.id && d.target.id === a.target.id)
-        );
-      });
-    }
-
-    function ticked() {
+  var circle = node
+    .append("circle")
+    .attr("r", function (d) {
+      return d.size;
+    })
+    .attr("fill", "#fff")
+    .style("stroke-width", 2)
+    .style("stroke", function (d) {
+      return color(d.color);
+    })
+    .on("mouseenter", (evt, d) => {
+      let list = [];
       link
-        .attr("x1", function (d) {
-          return d.source.x;
+        //.attr("display", "none")
+        .style("opacity", "0.2")
+        .filter((l) => {
+          if (l.source.index === d) {
+            list.push(l.target.id);
+            return true;
+          } else if (l.target.index === d) {
+            list.push(l.source.id);
+            return true;
+          }
+          return false;
         })
-        .attr("y1", function (d) {
-          return d.source.y;
-        })
-        .attr("x2", function (d) {
-          return d.target.x;
-        })
-        .attr("y2", function (d) {
-          return d.target.y;
-        });
+        .style("opacity", "1");
 
-      node.attr("transform", (d) => { return "translate(" + d.x + "," + d.y + ")"});
-    }
+      node
+        .style("opacity", "0.4")
+        .filter((n) => {
+          return n.index === d || list.includes(n.id);
+        })
+        .style("opacity", "1");
+    })
+    .on("mouseleave", (evt) => {
+      //link.attr("display", "block");
+      link.style("opacity", "1");
+      node.style("opacity", "1");
+    });
+
+  // Adding the label for each node
+  var text = node
+    .append("text")
+    .attr("dy", ".35")
+    .attr("dx", 12)
+    .attr("class", "unselectable")
+    .text(function (d) {
+      return d.id;
+    })
+    .attr("text-anchor", "middle");
+
+  node.append("title").text(function (d) {
+    return d.id;
   });
+
+  simulation.nodes(graph.nodes).on("tick", ticked);
+
+  simulation.force("link").links(graph.links);
+
+  // Finding connected people, usefull for later adding line highlighting
+  function neighboring(a, b) {
+    return graph.links.some(function (d) {
+      return (
+        (d.source.id === a.source.id && d.target.id === b.target.id) ||
+        (d.source.id === b.source.id && d.target.id === a.target.id)
+      );
+    });
+  }
+
+  function ticked() {
+    link
+      .attr("x1", function (d) {
+        return d.source.x;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("x2", function (d) {
+        return d.target.x;
+      })
+      .attr("y2", function (d) {
+        return d.target.y;
+      });
+
+    node.attr("transform", (d) => {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+  }
 
   // Used to drag the graph round the screen
   function dragstarted(d) {
